36595272a437ab06db7546bc70737fa2059a81ce
Abikwa <Abikalex8@gmail.com>
2021-01-30 15:34:27 +0200

    add  constant

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+define('DS',DIRECTORY_SEPARATOR);
+define('APP_ROOT',dirname(dirname(__FILE__)));
+define('URL_ROOT','http://localhost/bibliotheque');
+define('CONTROLLERS',APP_ROOT . DS . 'controller' . DS);
+define('LIBRARIES',APP_ROOT . DS . 'libraries' . DS);
+define('MODELS',APP_ROOT . DS . 'models'. DS);
+define('VIEWS',APP_ROOT . DS . 'view' . DS);
+define('INITIALS',APP_ROOT . DS . 'view' . DS . 'initial' . DS);
+define('TEMPLATE',APP_ROOT . DS . 'view' . DS . 'template' . DS);
