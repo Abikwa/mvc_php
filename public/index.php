0c7d872e724052a41979e50c18374daad3b6c788
Abikwa <Abikalex8@gmail.com>
2021-02-06 20:01:38 +0200

    update  public index

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 error_reporting(-1);
-use bibliotheque\app\libraries\Core\Core;
+use mvc_php_crud\app\libraries\Core\Core;
 error_reporting(-1);
 ini_set('display_errors','On');
 require_once '../app/init.php ';
