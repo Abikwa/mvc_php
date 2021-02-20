f25f4c8821061d43c5888e064a41a33d376a2738
Abikwa <Abikalex8@gmail.com>
2021-02-20 07:34:04 +0200

    add url helper file to redirect pages

--- app/init.php
+++ app/init.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'config/config.php';
+require_once 'helper/url_helper.php';
 spl_autoload_register(function($class){
  $parts=explode('\\',$class); 
   require_once LIBRARIES . end($parts) . '.php';
